05a5e58d9ef2f66f3376afe592cc1a84f06f61f6
Zorix <zoriks@gmail.com>
2015-05-20 03:42:01 +0300

    Update dru335cat.lua

--- dru335cat.lua
+++ dru335cat.lua
@@ -19,3 +19,29 @@
 /run if range("Волшебный огонь (зверь)")==1 and AmIBehind==false and not UnitBuff("player","Крадущийся зверь") then cst("Волшебный огонь (зверь)") end
 /script UIErrorsFrame:Clear();
 -- END
+#showtooltip Звериная атака - кошка
+/script UIErrorsFrame:Clear();
+-- VARIABLES
+/run ucf=UnitCastingInfo cst=CastSpellByName cd=GetSpellCooldown buf=UnitBuff dbuf=UnitDebuff p="player" t="target" range=IsSpellInRange spd=GetUnitSpeed hp=UnitHealth("player")/UnitHealthMax("player") mana=UnitPower("player")/UnitPowerMax("player")
+/script UIErrorsFrame:Clear();
+-- ATK
+/run if AmIBehind==true then cst("Полоснуть") end
+/run if range("Звериная атака - кошка",t)==1 and cd("Звериная атака - кошка")==0 then cst("Звериная атака - кошка") end
+/script UIErrorsFrame:Clear();
+/run if range("Цапнуть",t)==1 and cd("Тигриное неистовство")==0 then cst("Тигриное неистовство") end
+/script UIErrorsFrame:Clear();
+/run if range("Разорвать")==1 and GetComboPoints(p)>2  and not dbuf(t,"Разорвать") then cst("Разорвать") end
+/script UIErrorsFrame:Clear();
+/run if range("Глубокая рана")==1 and not dbuf(t,"Глубокая рана") then cst("Глубокая рана") end
+/script UIErrorsFrame:Clear();
+/run if range("Цапнуть")==1 and AmIBehind==false then cst("Цапнуть") end
+/script UIErrorsFrame:Clear();
+/run if range("Волшебный огонь (зверь)")==1 and AmIBehind==false and not UnitBuff("player","Крадущийся зверь") then cst("Волшебный огонь (зверь)") end
+/script UIErrorsFrame:Clear();
+-- END
+---------------------------------20/05/2015
+-- Работающий behindCheck
+/run if not behindCheck then behindCheck = CreateFrame("Frame")end
+/run local frame = behindCheck frame:RegisterEvent("UI_ERROR_MESSAGE") frame:SetScript("OnEvent", function(self, event, ...)if (select(1,...):find("позади") ~= nil) then CastSpellByName("Цапнуть") end end)
+/run CastSpellByName("Полоснуть");
+-- END
