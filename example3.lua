44179fd0116fc40def9e7d3e506dfd0e8f8014cc
Zorix <zoriks@gmail.com>
2015-05-26 11:33:13 +0300

    Create example3.lua

---
+++ example3.lua
@@ -0,0 +1,23 @@
+unction Shield(self, elapsed)   
+if UnitBuff("target", "Гнев карателя") then                         
+                         local enabled = GetSpellCooldown("Мощный удар щитом")                         
+                         if enabled == 0 then                         
+                          RunMacro("Макрос")                       
+                         end                         
+end                             
+end                         
+CreateFrame("Frame", "ShieldFrame")   
+ShieldFrame:SetScript("OnUpdate", Shield)
+---------------
+function Destroy(self, elapsed) 
+local enabled = GetSpellCooldown("Удар героя(Уровень 13)") 
+local rage = UnitPower("player") 
+local inRange = IsSpellInRange("Удар героя(Уровень 13)", "target") 
+if enabled == 0 and inRange == 1 and rage >= 79 then 
+RunMacroText("/cast Удар героя(Уровень 13)") 
+end 
+end 
+CreateFrame("Frame", "DestroyFrame") 
+DestroyFrame:SetScript("OnUpdate", Destroy)
+
+Раги >= 79 скрипт включается, меньше не включается
