6ebf219b7c4f8299d539397daca2fbfe64128bb9
Zorix <zoriks@gmail.com>
2015-05-26 11:34:33 +0300

    Update examples3.lua

--- examples3.lua
+++ examples3.lua
@@ -21,3 +21,18 @@ CreateFrame("Frame", "DestroyFrame")
 DestroyFrame:SetScript("OnUpdate", Destroy)
 
 Раги >= 79 скрипт включается, меньше не включается
+---------------------------------
+Ребят, можно ли сделать скрипт на вара, чтобы скил Мощный удар щитом, диспелил с цели определенный баф. 3.3.5 
+Вот так сделай 
+Код
+function Shield(self, elapsed)   
+if UnitBuff("target", "Гнев карателя") then                         
+                         local enabled = GetSpellCooldown("Мощный удар щитом")                         
+                         if enabled == 0 then                         
+                          RunMacro("Макрос")                       
+                         end                         
+end                             
+end                         
+CreateFrame("Frame", "ShieldFrame")   
+ShieldFrame:SetScript("OnUpdate", Shield)
+------------------------------
