ede6c9aeea83f7d7d5b6a779cb5bfd0157b1f16e
Zorix <zoriks@gmail.com>
2015-05-31 23:57:14 +0300

    Create fmage.lua

---
+++ PQR/fmage.lua
@@ -0,0 +1,11 @@
+if IsSpellInRange(GetSpellInfo(30455), "target") == 1
+and UnitExists("target")  ==1
+and UnitIsVisible("target") ==1
+then
+if UnitDebuffID("target",27088,"player") ~= nil
+or UnitDebuffID("target",33395,"player") ~= nil
+or UnitDebuffID("target",12494,"player") ~= nil
+or UnitBuffID("player", 44545) ~= nil
+then return true
+end
+end
